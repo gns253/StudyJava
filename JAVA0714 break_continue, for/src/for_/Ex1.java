79873db3e1c6b6507013ae3d08112b7f47e9fa34
gns253 <gns253@naver.com>
2022-09-22 15:26:36 +0900

    0714

---
+++ JAVA0714 break_continue, for/src/for_/Ex1.java
@@ -0,0 +1,45 @@
+package for_;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		
+		for(int min = 0; min < 60; min++) {
+			
+			for(int sec = 0; sec < 60; sec ++) {
+				System.out.println(min +"분"+sec+"초");
+			}
+		}
+		
+		
+		for(int h = 0; h < 24; h++) {
+			
+			for(int m = 0; m < 60; m ++) {
+				for(int s = 0; s < 60; s ++) {
+					System.out.println(h +"시"+m+"분"+s+"초");
+					
+				}	
+			
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ JAVA0714 break_continue, for/src/for_/Test2.java
@@ -0,0 +1,37 @@
+package for_;
+
+import java.util.Scanner;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		
+		Scanner sc = new Scanner(System.in);
+		int n =sc.nextInt();
+		int line = n;
+		int space = 0;
+	
+		for(int i = 0; i < line; i++) {
+			for(int j = 0; j < space; j++) {
+				System.out.print(" ");
+			}
+				System.out.println("**");
+			
+				space++;
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
